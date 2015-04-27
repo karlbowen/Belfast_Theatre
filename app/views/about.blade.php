c83ccd42f92d5e537ef3461dd24bad0a1a4bc0cf
karlbowen <bowen-k@email.ulster.ac.uk>
2015-04-27 15:35:14 +0100

    Just checking

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -14,12 +14,12 @@
       
         <div class="panel-body">
             
-         <h5> Our goal </h5> 
-         
+            <h5><b> Our goal</b></h5>
+         Our staff are dedicated towards making sure every customer get's guaranteed satisfaction and 100% value for money. Set up by the McClelland family in the late 1980's the Belfast Theatre has been a family affair ever since and thrived even during Belfast's dark times bringing everyone that bit closer together through their love for the theatre. Our goal is to allow every theatregoer,may it be their first time or if they have been going for years', the chance to enjoy their selected play in a comfortable and relaxed environment while at the same time allow them to have their soul enriched by play<br>
          <p> </p>
          
-         <h5> Location </h5>  
-         Situated on the northernly side of Belfast's Golden Mile the Belfast Theatre is situated on Great Victoria Street close to the famous Europa Hotel. <br>
+         <h5><b> Location</b> </h5>  
+         Situated on the northernly side of Belfast's Golden Mile the Belfast Theatre is situated on College Square East close to the world famous Europa Hotel and the ageless Grand Opera.<br>
            
          <p> </p> 
          
